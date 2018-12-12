86afebd7de9b1523d4fc19f8d4da0e74d03f8115
JuhaszAlex <juhaszalex95@gmail.com>
2018-12-13 00:46:28 +0100

    Updates

---
+++ precache-manifest.d3cc3e65a048fdce87fb255e046b5da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c73e9e484c1eabce9855",
+    "url": "/staffregistry/static/js/runtime~main.c73e9e48.js"
+  },
+  {
+    "revision": "61efd968af8042ff4b15",
+    "url": "/staffregistry/static/js/main.61efd968.chunk.js"
+  },
+  {
+    "revision": "e74bbb01f853c701e501",
+    "url": "/staffregistry/static/js/1.e74bbb01.chunk.js"
+  },
+  {
+    "revision": "61efd968af8042ff4b15",
+    "url": "/staffregistry/static/css/main.b1bed0a1.chunk.css"
+  },
+  {
+    "revision": "e74bbb01f853c701e501",
+    "url": "/staffregistry/static/css/1.6d8cf3c1.chunk.css"
+  },
+  {
+    "revision": "d3c27d92e935eded1a50c9769117b80b",
+    "url": "/staffregistry/index.html"
+  }
+];
